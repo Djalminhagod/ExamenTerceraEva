3e83729315f81f67f3f13d2e0dd7f3112e7abb23
Djalminhagod <r.senra@liceolapaz.net>
2026-06-04 09:30:36 +0200

    tercer Commit haciendo parte depedir profesional

--- src/club/GestorClub.java
+++ src/club/GestorClub.java
@@ -1,11 +1,81 @@
 package club;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class GestorClub {
     private final double PRESUPUESTO_MAXIMO = 10000.0;
 
     ArrayList<Profesional> trabajadores = new ArrayList<>();
 
+    public void controlFlujo() throws PresupuestoExcedidoException {
+        Scanner sc = new Scanner(System.in);
+        int opcion;
+        double salarioTotal = 0;
+        while (true) {
+            boolean salida=true;
+            menu();
+            opcion = sc.nextInt();
+            switch (opcion) {
+                case 1:
+                    do {
 
+                        System.out.println("Di Nombre Jugador");
+                        String nombre = sc.next();
+                        System.out.println("Di Salario Jugador");
+                        double salario = sc.nextDouble();
+                        System.out.println("Di Posicion Jugador (PORTERO, DEFENSA, CENTROCAMPISTA, DELANTERO)");
+                        String posicion = sc.next();
+                        trabajadores.add(new Jugador(nombre,salario,Posicion.valueOf(posicion.toUpperCase())));
+                        salarioTotal+=salario;
+                        if(salarioTotal>PRESUPUESTO_MAXIMO) {
+                            throw new PresupuestoExcedidoException("presupuesto Excedido");
+                        }
+
+                            System.out.println("Quieres crear otro jugador? S/N");
+                            String salir = sc.next();
+                            if (salir.equals("N") || salir.equals("n")) {
+                                salida = false;
+                            }
+
+
+                        }while(salida);
+
+                case 2:
+                    do {
+
+
+                        System.out.println("Di Nombrer tecnico");
+                        String nombre = sc.next();
+                        System.out.println("Di Salario  tecnico");
+                        double salario = sc.nextDouble();
+                        System.out.println("Di Posicion  tecnico (Ej: Entrenador, Fisioterapeuta,Médico, Analista etc)");
+                        String puesto = sc.next();
+                        trabajadores.add(new Tecnico(nombre, salario, puesto.toUpperCase()));
+                        salarioTotal += salario;
+                        if (salarioTotal > PRESUPUESTO_MAXIMO) {
+                            throw new PresupuestoExcedidoException("presupuesto Excedido");
+                        }
+                        System.out.println("Quieres crear otro tecnico? S/N");
+                        String salir = sc.next();
+                        if (salir.equals("N") || salir.equals("n")) {
+                            salida = false;
+                        }
+
+                    }while (salida);
+                    }
+            }
+
+
+
+
+        }
+    public static void menu(){
+        System.out.println("1. Contratar Jugador");
+        System.out.println("2. Contratar cuerpor tecnico");
+        System.out.println("3. Despedir profesional");
+        System.out.println("4. Ver nominas totales");
+        System.out.println("5. Salir");
+    }
 }
+
--- src/club/Main.java
+++ src/club/Main.java
@@ -4,39 +4,11 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
-    public static void main() {
-        Scanner sc = new Scanner(System.in);
-        int opcion;
-        while (true) {
-            menu();
-            opcion = sc.nextInt();
-            switch (opcion) {
-                case 1:
-                    ArrayList<Jugador> jugadores = new ArrayList<>();
+    public static void main() throws PresupuestoExcedidoException {
 
-                    jugadores.add(new Jugador("Yeremay",2000,Posicion.DELANTERO));
-                    break;
-
-                case 2:
-
-
-
-
-            }
-        }
-
-
+        GestorClub gestorClub = new GestorClub();
+        gestorClub.controlFlujo();
 
 
     }
-    public static void menu(){
-        System.out.println("1. Contratar Jugador");
-        System.out.println("2. Contratar cuerpor tecnico");
-        System.out.println("3. Despedir profesional");
-        System.out.println("4. Ver nominas totales");
-        System.out.println("5. Salir");
-    }
-
-
-
 }
